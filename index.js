0b75ed37807fdf90777d322a120c57e548d62129
Winson Quon <winguy2003@gmail.com>
2019-11-21 18:39:00 -0500

    add github api lookup

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const questions = [
+const axios = require("axios");
+const inquirer = require("inquirer");
+// const puppeteer = require("puppeteer");
 
+function validateName(name) {
+    return name !== '';
+}
+
+const questions = [
+    {
+        message: "GitHub Username?",
+        type: "input",
+        name: "username",
+        validate: validateName
+    },
+    {
+        message: "Color?",
+        type: "list",
+        name: "color",
+        choices: ["Red", "Green", "Blue", "Pink"]
+    }
 ];
 
+/*
 function writeToFile(fileName, data) {
 
 }
+*/
 
-function init() {
-
+async function init() {
+    try {
+        const { username, color } = await inquirer.prompt(questions);
+        const { data } = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 init();
