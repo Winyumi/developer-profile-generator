bdaef6b18369fbaa667816b284894d6dae0c3194
Winyumi <winguy2003@hotmail.com>
2019-11-24 17:44:45 -0500

    recombine colors into generateHTML.js

--- modules/generateHTML.js
+++ modules/generateHTML.js
@@ -1,3 +1,30 @@
+const colors = {
+  green: {
+    wrapperBackground: "#E6E1C3",
+    headerBackground: "#C1C72C",
+    headerColor: "black",
+    photoBorderColor: "#black"
+  },
+  blue: {
+    wrapperBackground: "#5F64D3",
+    headerBackground: "#26175A",
+    headerColor: "white",
+    photoBorderColor: "#73448C"
+  },
+  pink: {
+    wrapperBackground: "#879CDF",
+    headerBackground: "#FF8374",
+    headerColor: "white",
+    photoBorderColor: "#FEE24C"
+  },
+  red: {
+    wrapperBackground: "#DE9967",
+    headerBackground: "#870603",
+    headerColor: "white",
+    photoBorderColor: "white"
+  }
+};
+
 function generateHTML(data) {
   return `<!DOCTYPE html>
 <html lang="en">
@@ -146,4 +173,4 @@ function generateHTML(data) {
       </style>`;
 }
 
-module.exports = generateHTML;
+module.exports = { colors, generateHTML };
